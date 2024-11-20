68e03ce08eac84ead9b4bff59f76ba112f8886a8
zizhao <854945300@qq.com>
2024-11-20 11:38:08 +0800

    fix: 修复输入问题

--- src/views/product-edit/Desc.vue
+++ src/views/product-edit/Desc.vue
@@ -4,34 +4,14 @@
     :required="true"
     label="产品描述"
     placeholder="描述一下产品的信息～"
-    readonly
-    is-link
-    @click="clickHandle"
+    type="textarea"
+    :maxlength="300"
     :rules="[{required: true, message: '产品描述不能为空'}]"
   />
-  <VanDialog
-    class="com-product-edit__desc"
-    v-model:show="show"
-    teleport="body"
-    show-cancel-button
-    :beforeClose="beforeCloseHandle"
-  >
-    <div class="content">
-      <VanField
-        ref="fieldRef"
-        v-model="tmpVal"
-        rows="1"
-        autosize
-        type="textarea"
-        placeholder="请输入产品描述～"
-      />
-    </div>
-  </VanDialog>
 </template>
 
 <script setup>
-import { ref, computed } from 'vue'
-import { showToast } from 'vant';
+import { computed } from 'vue'
 
 const props = defineProps({
   modelValue: {type: String}
@@ -39,49 +19,16 @@ const props = defineProps({
 
 const emits = defineEmits(['update:modelValue'])
 
-const valDisplay = computed(() => {
-  const {modelValue} = props
-  return modelValue
-})
-
-const fieldRef = ref()
-const show = ref(false)
-const tmpVal = ref()
-
-const clickHandle = () => {
-  show.value = true
-  tmpVal.value = props.modelValue
-  setTimeout(() => {
-    fieldRef.value.focus()
-  }, 300);
-}
-
-const beforeCloseHandle = (action) => {
-  if (action === 'cancel') return true
-  let val = tmpVal.value || ''
-  val = val.trim()
-  if (!val) {
-    showToast('描述不能为空～')
-    return false
+const valDisplay = computed({
+  get() {
+    return props.modelValue
+  },
+  set(val) {
+    emits('update:modelValue', val)
   }
-  emits('update:modelValue', tmpVal.value)
-  return true
-}
+})
 
 
 
 </script>
 
-
-<style lang="scss" scoped>
-.com-product-edit__desc {
-  .content {
-    height: 50vh;
-    overflow: auto;
-    :deep(textarea) {
-      min-height: 50vh;
-    }
-  }
-}
-
-</style>
