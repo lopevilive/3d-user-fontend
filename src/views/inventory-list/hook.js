059b10d43cf5c44bbdc48b6db0294eaf94c4cd44
zizhao <854945300@qq.com>
2025-01-09 01:01:27 +0800

    fix: bug fix

--- src/views/inventory-list/hook.js
+++ src/views/inventory-list/hook.js
@@ -123,11 +123,16 @@ export const useInventoryList = () => {
         address = `${item.province}${item.city}${item.county}${item.addressDetail} ${item.name} ${item.tel}`
       }
     }
+    const list = shopCarList.value.map((item) => {
+      return { ...item,
+        desc: item.desc.replaceAll(/[\uD83C|\uD83D|\uD83E][\uDC00-\uDFFF][\u200D|\uFE0F]|[\uD83C|\uD83D|\uD83E][\uDC00-\uDFFF]|[0-9|*|#]\uFE0F\u20E3|[0-9|#]\u20E3|[\u203C-\u3299]\uFE0F\u200D|[\u203C-\u3299]\uFE0F|[\u2122-\u2B55]|\u303D|[\A9|\AE]\u3030|\uA9|\uAE|\u3030/ig, '')
+      }
+    })
 
     const payload = {
       shopId,
       data: {
-        list: shopCarList.value,
+        list: list,
         remark: globalData.value.invertoryRemark || '',
         address,
         totalCount: totalCount.value,
