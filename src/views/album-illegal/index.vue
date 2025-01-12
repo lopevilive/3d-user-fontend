a69f68dd549711b5aa498ee4744dc3be0dc769f3
zizhao <854945300@qq.com>
2025-01-12 23:48:32 +0800

    feat: 体验优化

--- src/views/album-illegal/index.vue
+++ src/views/album-illegal/index.vue
@@ -1,9 +1,23 @@
 <template>
   <div class="view-album-illegal">
-    <van-empty description="你似乎来到了没有内容存在的荒原～" />
+    <VanEmpty description="你似乎来到了没有内容存在的荒原～" />
+    <VanButton type="primary" block text="回到首页" @click="goHome"/>
   </div>
 </template>
 
 <script setup>
+import { useRouter } from 'vue-router'
 
+const router = useRouter()
+
+const goHome = () => {
+  router.replace({name: 'home'})
+}
 </script>
+
+<style lang="scss" scoped>
+.view-album-illegal {
+  padding: 0 $pdH;
+}
+
+</style>
