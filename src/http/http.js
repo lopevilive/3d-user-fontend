05f010290805fa93394a4ba52512c29f15a5f632
zizhao <854945300@qq.com>
2025-03-25 14:41:50 +0800

    feat: 更换域名

--- src/http/http.js
+++ src/http/http.js
@@ -6,7 +6,7 @@ import { toLogin } from '@/util'
 const http = axios.create({
   timeout: 1000 * 10,
   withCredentials: true,
-  baseURL: '//xiaoguo.mkeg.cn'
+  baseURL: '//album.xiaoguoyun.top'
 });
 
 // 请求拦截器
