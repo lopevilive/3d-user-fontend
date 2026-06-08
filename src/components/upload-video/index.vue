573b5713ff89e4320d9d0e70a1d71d63fd9f93a3
zizhao <854945300@qq.com>
2026-06-08 23:29:02 +0800

    feat: 视频放宽大小

--- src/components/upload-video/index.vue
+++ src/components/upload-video/index.vue
@@ -211,7 +211,7 @@ const delHandle = async () => {
 
 const maxSize = computed(() => {
   const { videoLimitS = 0 } = globalData.value.usage
-  let ret = videoLimitS * 1.5 * 1024 * 1024
+  let ret = videoLimitS * 3 * 1024 * 1024 // 1 秒最多给 4 m
   // return 1024 * 1024 * 1
   return ret || 50 * 1024 * 1024
 })
