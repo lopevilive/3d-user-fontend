6f828fbb16ea52bb3108cb305f560018aa96fb45
zizhao <854945300@qq.com>
2026-06-22 22:24:48 +0800

    feat: 非会员暂时不支持视频

--- src/views/product-edit/hooks.js
+++ src/views/product-edit/hooks.js
@@ -147,9 +147,8 @@ export const useProductEdit = () => {
 
   const isShowVideo = computed(() => {
     if (!shopId) return false
+    if (shopInfo.value.level === 0) return false // 非会员暂时不支持视频
     return true
-    // if ([5,8].includes(shopId)) return true
-    // return false
   })
 
   
